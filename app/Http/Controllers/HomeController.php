16299bc4c917c7e16aef6da3ee05fde2269787e6
DESKTOP-UD5VIRK\rela_ <72004515+oeuser@users.noreply.github.com>
2026-01-29 11:23:05 +0800

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Crypt;
@@ -31,6 +31,10 @@ class HomeController extends Controller
         $id = decrypt($encryptid);
         User::findOrFail($id)->delete();
 
+        Log::info('User delete triggered', [
+            'user_id' => $id
+        ]);
+
         return redirect()->back()->with('success', 'Data berjaya dipadam');
     }
 
@@ -38,6 +42,10 @@ class HomeController extends Controller
     {
         $id = decrypt($encryptid);
         User::findOrFail($id)->delete();
+
+        Log::info('User delete triggered', [
+            'user_id' => $id
+        ]);
 
         return redirect()->back()->with('success', 'Data berjaya dipadam');
     }
